69d210010209ae068e76570d9047d6a9fc21b9da
ykwuana <ykwuana@gmail.com>
2024-01-19 17:25:32 +0700

    initial comit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+# percabangan
+
+jumlah_botol_susu = input('berapa jumlah susu ?')
+jual_telur = input('apakah ada telur juga?')
+
+if jumlah_botol_susu > '0':
+    print('budi membeli 1 botol susu')
+    if jual_telur == 'ada':
+        print('budi membeli telur 6 butir')
+    else:
+        print('budi tidak membeli telur')
+else:
+    print('budi tidak jadi membeli 1 botol susu dan telur')
+
+
+
+
+
+
+
+
+
+
