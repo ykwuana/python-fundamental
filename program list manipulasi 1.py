7bafd2c7d155c42006e73b7142c9939e0003c845
ykwuana <ykwuana@gmail.com>
2024-01-23 09:16:18 +0700

    mantap

---
+++ program list manipulasi 1.py
@@ -0,0 +1,95 @@
+## Operasi
+
+# index 0(-3)     1(-2)    2(-1)
+data = ["ucup", "otong", "dudung"]
+
+ # mengambil data dari list ini
+print(25*'=')
+print('untuk mengambil data dari list ini')
+print(25*'=' + '\n')
+data_0 = data[0]
+print(f'data pertama (index 0) = {data_0}')
+
+print(f'\n{10*'+'} ATAU {10*'+'}\n')
+print(f'nama ucup ada diurutan ke = {data.index('ucup')}\n ')
+
+
+
+# mengambil info jumlah data dalam list
+print(25*'=')
+print('untuk mencari berapa panjang/jumlah data')
+print(25*'=' + '\n')
+
+panjang_data = len(data)
+print(f'panjang data = {panjang_data}\n')
+
+# menambahkan item pada list sesuai posisi
+print(25*'=')
+print('untuk menambah data')
+print(25*'=' + '\n')
+
+print(f'data sebelum ditambah = {data}')
+
+data.insert(1,'asep')                #(posisi,item)
+print(f'data setelah ditambah = {data}\n')
+
+# nambahnya langsung di akhir list
+print(25*'=')
+print('untuk menambah data di akhir list')
+print(25*'=' + '\n')
+
+data.append('jajang')
+print((f'data ditambah lagi = {data}\n'))
+
+# menambah list dengan list
+print(25*'=')
+print('untuk menambah list dengan list')
+print(25*'=' + '\n')
+
+data_baru = ['ujang', 'usep', 'dadang']
+data.extend(data_baru)
+print(f'data gabungan {data}\n')
+
+# merubah data
+print(25*'=')
+print('untuk merubah data')
+print(25*'=' + '\n')
+
+data[2] = 'michael'
+print(f'data rubah = {data}\n')
+
+# me remove data
+print(25*'=')
+print('untuk meremove salah satu data')
+print(25*'=' + '\n')
+
+data.remove('ujang')
+print(f'data remove {data}\n')
+
+
+
+# meremove data paling akhir
+print(25*'=')
+print('untuk meremove data paling akhir')
+print(25*'=' + '\n')
+
+data_akhir = data.pop()  # bisa juga langsung spt >>>>> data.pop()>>>>>>tapi ini untuk pop aj
+print(f'data akhir diremove {data}')
+
+print(f'nama akhir yang di hapus = {data_akhir}\n')
+
+#untuk menghapus data yang di tuju
+print(25*'=')
+print('untuk menghapus data yang di tuju')
+print(25*'=' + '\n')
+
+del data[0]
+print(f'{data}\n')
+
+#untuk meghapus semua data
+print(25*'=')
+print('untuk meghapus semua data')
+print(25*'=' + '\n')
+
+data.clear()
+print(f'{data}')
---
+++ program list manipulasi 2.py
@@ -0,0 +1,56 @@
+# cara lain untuk output list nya
+print(25*'=')
+print('cara alternatif untuk output list')
+print(25*'=' + '\n')
+
+list_example = [30, 33, 50, 75, 40, 77, 60]
+for item in list_example:
+    print(item)
+
+print('\n')
+#atau buat cari yang genap
+print(25*'=')
+print('untuk mencari yang genap')
+print(25*'=' + '\n')
+
+for angka in list_example:
+    if angka % 2 == 0:
+        print(f'{angka}\n')
+
+# atau untuk mencari item tertentu
+print(25 * '=')
+print('untuk mencari item tertentu')
+print(25 * '=' + '\n')
+
+
+if 33 in list_example:
+    print(f'ada angka 33 di list example = {33}\n')             # kalo gk ada itemnya, gk keluar angkanya
+
+# untuk copy list dengan identity yg berbeda
+
+print(25 * '=')
+print('untuk copy list dengan identity yg berbeda')
+print(25 * '=' + '\n')
+
+data_list_1 = [60, 20, 30, 15, 27, 9]
+data_list_2 = data_list_1.copy()
+data_list_2.insert(3, 77)
+data_list_2[4] = 13                        # di ganti
+print(data_list_1)
+print(data_list_2)
+
+# untuk mengurutkan
+print(25*'=')
+print('untuk mengurutkan')
+print(25*'=' + '\n')
+
+data_list_2.sort()
+print(f'setelah diurutkan = {data_list_2}\n')
+
+# untuk dibalik
+print(25*'=')
+print('untuk di balik')
+print(25*'=' + '\n')
+
+data_list_2.reverse()
+print(f'setelah dibalik = {data_list_2}\n')
